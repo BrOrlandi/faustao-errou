7ff58b9da322b7d0514c545a4c0ccbb8ad823692
BrOrlandi <brorlandi@gmail.com>
2016-11-14 23:03:05 -0200

    Node module finished

--- forWindows.vbs
+++ forWindows.vbs
@@ -2,7 +2,7 @@ Dim oPlayer
 Set oPlayer = CreateObject("WMPlayer.OCX")
 
 ' Play audio
-oPlayer.URL = "C:\welcome.mp3"
+oPlayer.URL = WScript.Arguments(0)
 oPlayer.controls.play
 While oPlayer.playState <> 1 ' 1 = Stopped
   WScript.Sleep 100
--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+#!/usr/bin/env node
 
 const exect = require('child_process').exec;
+const path = require('path');
+const fs = require('fs');
+
+var soundPath = path.join(path.dirname(fs.realpathSync(__filename)), './faustao-errou');
 
 const faustaoErrou = function (){
-    const linuxcmd = 'paplay faustao-errou.ogg';
-    const windowscmd = 'forWindows.vbs';
+    const linuxcmd = 'paplay '+soundPath+'.ogg';
+    const windowscmd = 'forWindows.vbs '+soundPath+'.mp3';
 
     const platform = process.platform;
 
     if(platform === 'linux'){
-        exec(linuxcmd);
+        return exec(linuxcmd);
     }
     else if(platform === 'win32'){
-        exec(windowscmd);
+        return exec(windowscmd);
     }
 
     function exec(cmd){
-        exect(cmd, function (error, stdout, stderr) {
+        return exect(cmd, function (error, stdout, stderr) {
            console.log(stdout);
+           if(error)
+               console.error(error);
         });
     }
 }
